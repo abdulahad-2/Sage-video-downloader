f473a15401c6d06e9b827ca8ac861c91932b9a9b
Abdul Ahad <ahadking6152@gmail.com>
2025-08-11 00:29:56 -0700

    solved render error

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // Enables Static HTML Export
-  env: {
-    NEXT_PUBLIC_API_BASE: "http://127.0.0.1:8000",
-  },
   /* config options here */
 };
 
